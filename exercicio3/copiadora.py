66f37a836d98d3283e9771998c5a3748de93f7aa
João P Silva <jpsilva313@gmail.com>
2025-03-25 23:03:07 -0300

    add exercicio copiadora

---
+++ exercicio3/copiadora.py
@@ -0,0 +1,18 @@
+
+
+def escolha_servico():
+    while True:
+        print('Entre com o tipo de serviço desejado')
+        print('DIG - Digitalização')
+        print('ICO - Impressão colorida')
+        print('IPB - Impressão Preto e Branco')
+        print('FOT - Fotocópia')
+        servico = input('>> ').lower()
+        
+        if servico == 'dig':
+            return 1.10
+    
+def num_pagina():
+    
+
+def servico_extra():
